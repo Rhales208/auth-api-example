432c42c387b2713c6bd0eb62b95c8559f5d39f1b
RuebenHales <halesrueben@gmail.com>
2022-02-28 11:53:20 -0500

    sample api

---
+++ src/dbConnect.js
@@ -0,0 +1,12 @@
+const { initializeApp, cert, getApps } = require('firebase-admin/app')
+const{getFirestore} = require('firebase-admin/firestore')
+const credentials= require('../credentials.json')
+
+exports.connectDb = () => {
+    if(!getApps.length) {
+        initializeApp({
+            credential: cert(credentials)
+        })
+    }
+    return getFirestore()//this is base of how to intialize to firestore
+}
