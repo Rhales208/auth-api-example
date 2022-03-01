4ea1c7b3281313afd8a087e894659038ff8c0045
RuebenHales <halesrueben@gmail.com>
2022-03-01 10:14:23 -0500

    auth api

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
-const cors= require('cors')
-const {createUser}= require('./src/users')
+const cors = require('cors')
+const { createUser, loginUser, getUsers } = require('./src/users')
 const PORT = process.env.PORT || 3001
 
-const app= express()
+const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post('/users', createUser)//i guess this is needed to post in postman
+app.post('/users', createUser)
+app.post('/users/login', loginUser)
+app.get('/users', getUsers)
 
-app.listen(PORT,() => {
-    console.log(`Listening on port ${PORT}...`)
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`)
 })
--- src/users.js
+++ src/users.js
@@ -1,23 +1,106 @@
-const {connectDb } = require('./dbConnect')
-exports.createUser = (req,res) => {
-    //first, lets do some validation... (email, password)
-    if(!req.body || !req.body.email || !req.body.password) {
-//invalid request
-res.status(400).send('Invalid request')
-return
-}
-const newUser = {
-email:req.body.email,
-password: req.body.password,
-isAdmin: false,
-userRole: 5,
+const { connectDb } = require('./dbConnect')
+
+exports.createUser = (req, res) => {
+  // first, let's do some validation... (email, password)
+  if(!req.body || !req.body.email || !req.body.password) {
+    // invalid request
+    res.status(400).send({
+      success: false,
+      message: 'Invalid request',
+    })
+    return
+  }
+  const newUser = {
+    email: req.body.email.toLowerCase(),
+    password: req.body.password,
+    isAdmin: false,
+    userRole: 5,
+  }
+  const db = connectDb()
+  db.collection('users').add(newUser)
+    .then(doc => {
+      const user = { // this will become the payload for our JWT
+        id: doc.id,
+        email: newUser.email,
+        isAdmin: false,
+        userRole: 5,
+      }
+      // TODO: create a JWT and send back the token
+      res.status(201).send({
+        success: true,
+        message: 'Account created',
+        token: user, // add this to token later
+       })
+    })
+    .catch(err => res.status(500).send({ 
+      success: false,
+      message: err.message,
+      error: err 
+    }))
 }
 
-const db = connectDb()
-    db.collection('users').add(newUser)
-    .then(doc => {
-        //TODO: create a JWT and send back the token
-        res.status(201).send('Account created')  
+exports.loginUser = (req, res) => {
+  // first, let's do some validation... (email, password)
+  if(!req.body || !req.body.email || !req.body.password) {
+    // invalid request
+    res.status(400).send({
+      success: false,
+      message: 'Invalid request',
     })
-    .catch(err => res.status(500).send(err))
+    return
+  }
+  const db = connectDb()
+  db.collection('users')
+    .where('email','==', req.body.email.toLowerCase())
+    .where('password','==', req.body.password)
+    .get()
+      .then(snapshot => {
+        if(snapshot.empty) { // bad login
+          res.status(401).send({
+            success: false,
+            message: 'Invalid email or password',
+          })
+          return
+        }
+        // good login
+        const users = snapshot.docs.map(doc => {
+          let user = doc.data()
+          user.id = doc.id
+          user.password = undefined
+          return user
+        })
+        res.send({
+          success: true,
+          message: 'Login successful',
+          token: users[0]
+        })
+      })
+      .catch(err => res.status(500).send({ 
+        success: false,
+        message: err.message,
+        error: err 
+      }))
+}
+
+exports.getUsers = (req, res) => { // TODO: Protect this route with JWT
+  const db = connectDb()
+  db.collection('users').get()
+    .then(snapshot => {
+      const users = snapshot.docs.map(doc => {
+        let user = doc.data()
+        user.id = doc.id
+        user.password = undefined
+        return user
+      })
+      res.send({
+        success: true,
+        message: 'Users returned',
+        users
+      })
+    })
+    .catch(err => res.status(500).send({ 
+      success: false,
+      message: err.message,
+      error: err 
+    }))
 }
